28432aa20f177bcc08b4895ffe793880ff3b3d0e
raghdarizq <raghdarizq2@gmail.com>
2023-04-26 20:44:36 +0300

    public

--- src/router/PageRouter.js
+++ src/router/PageRouter.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 
 PageRouter.get('/homePage', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "html", "homePage.html"))
+  res.sendFile(path.join(__dirname, "..", "..", "Public", "html", "homePage.html"))
 })
 PageRouter.get('/UserPage', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "html", "UserPage.html"))
+  res.sendFile(path.join(__dirname, "..", "..", "Public", "html", "UserPage.html"))
 })
 PageRouter.get('/singin', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "html", "singinPage.html"))
+  res.sendFile(path.join(__dirname, "..", "..", "Public", "html", "singinPage.html"))
 })
 PageRouter.get('/singup', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "..", "public", "html", "singupPage.html"))
+  res.sendFile(path.join(__dirname, "..", "..", "Public", "html", "singupPage.html"))
 })
 
 module.exports = PageRouter;
