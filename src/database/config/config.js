c16b083f6a621ccf0d45ab28395b342542e277c0
raghdarizq <raghdarizq2@gmail.com>
2023-04-26 12:14:41 +0300

    tod

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 
 require('dotenv').config();
 
-const { DB_URL, NODE_ENV } = process.env;
+const { DB_URL, DATABASE_URL, NODE_ENV } = process.env;
 
 const option = {
-  connectionString: DB_URL,
-  ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  connectionString: NODE_ENV  === 'production' ? DATABASE_URL : DB_URL,
+  ssl: NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 };
 
 const connection = new Pool(option);
